a23ecf4113e7a12b9e9028bdf4c64dda3f79b913
riri-jv <5jvrashmi@gmail.com>
2024-11-26 01:15:26 +0530

    update

--- src/mainapp/components/left-pane.jsx
+++ src/mainapp/components/left-pane.jsx
@@ -85,7 +85,7 @@ export const LeftPane = ({handleClick}) =>{
 
 
     <div className="contactus">
-  <a href="/rubik-s-cube-simulator-main/BusinessCard.html" target="_blank">
+  <a href="/rubik-s-cube-simulator-main/BusinessCard.html">
     <button className="Contact">Contact Us</button>
   </a>
 </div>
